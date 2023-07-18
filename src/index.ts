99544f187aaef8c6a64199800266d717494f29db
gwongibeom <k201gun@kakao.com>
2023-07-18 19:02:45 +0900

    week 3

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import express from 'express'
+import multer from 'multer'
+import fs from 'fs'
+
+// 1x1XL5wpRSyqqEGE
 
 const app = express()
-const port = 3300
-
-app.use('/static', express.static('static'))
+const upload = multer({ dest: 'static/' })
 
 interface User {
   name: string
@@ -12,15 +14,30 @@ interface User {
 
 const db: User[] = []
 
-console.log(db)
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(upload.fields([{ name: 'ooo' }]))
+app.use('/static', express.static('static'))
 
 app.get('/', (req, res) => {
   return res.json(db)
 })
 
 app.post('/', (req, res) => {
-  const name = req.query.name
-  const age = req.query.age
+  const name = req.body.name
+  const age = req.body.age
+
+  if (req.files === undefined) {
+    return res.send('Error:"files"가 유효하지 않습니다.')
+  }
+
+  if (!Array.isArray(req.files)) {
+    const { filename, originalname } = req.files.ooo[0]
+    fs.renameSync(
+      `./static/${filename}`,
+      `./static/${originalname}`
+    )
+  }
 
   if (typeof name !== 'string') {
     return res.send('Error:"name"은 string이 아닙니다.')
@@ -43,14 +60,15 @@ app.post('/', (req, res) => {
   return res.json(db)
 })
 
+app.post('/image-upload', (req, res) => {
+  return res.send('image uploaded')
+})
+
 app.put('/:targetName', (req, res) => {
   const targetName = req.params.targetName
   const name = req.query.name
   const age = req.query.age
 
-  if (typeof targetName !== 'string') {
-    return res.send('Error:"targetName"은 string이 아닙니다.')
-  }
   if (typeof name !== 'string') {
     return res.send('Error:"name"은 string이 아닙니다.')
   }
@@ -65,7 +83,7 @@ app.put('/:targetName', (req, res) => {
 
   const index = db.findIndex(user => user.name === targetName)
   if (db[index] === undefined) {
-    return res.send('Error:존재하지 않는 유저입니다.')
+    return res.send('Error:존재하지 않는 이름입니다.')
   }
 
   db[index].name = name
@@ -77,19 +95,17 @@ app.put('/:targetName', (req, res) => {
 app.delete('/:targetName', (req, res) => {
   const targetName = req.params.targetName
 
-  if (typeof targetName !== 'string') {
-    return res.send('Error:"targetName"은 string이 아닙니다.')
-  }
-
   const index = db.findIndex(user => user.name === targetName)
   if (db[index] === undefined) {
-    return res.send('Error:존재하지 않는 유저입니다.')
+    return res.send('Error:존재하지 않는 이름입니다.')
   }
-  delete db[index]
 
-  return res.json({})
+  db.splice(index, 1)
+
+  return res.json(db)
 })
 
+const port = 3300
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
