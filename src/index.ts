ae156cb0bdf0f94a052aa5ebf3f7527269909e7d
gwongibeom <k201gun@kakao.com>
2023-08-04 19:10:40 +0900

    과제

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,6 @@
-import express from 'express'
-import multer from 'multer'
 import { connectDB } from './db'
-import {
-  path as signupPath,
-  method as signupMethod,
-  handler as signupHandler
-} from './routes/signup'
+import { startSever } from './sever'
 
-async function startSever (): Promise<void> {
-  const port = 3300
-
-  const app = express()
-  const upload = multer({ dest: 'static/' })
-
-  app.use(express.json())
-  app.use(express.urlencoded({ extended: true }))
-  app.use(upload.fields([{ name: 'ooo' }]))
-  app.use('/static', express.static('static'))
-  app[signupMethod](signupPath, signupHandler)
-  app.post('/signup', () => {})
-
-  await new Promise((resolve) => {
-    app.listen(port, (): void => {
-      resolve(undefined)
-    })
-  })
-  console.log('server on')
-}
 async function main (): Promise<void> {
   await connectDB()
   await startSever()
